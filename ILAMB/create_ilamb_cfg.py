2b6bbf3d4be8436594e26a9b6972f15fbc6e0af4
Nathan Collier <nathaniel.collier@gmail.com>
2019-01-25 14:00:34 -0500

    initial commit

---
+++ ILAMB/create_ilamb_cfg.py
@@ -0,0 +1,40 @@
+from netCDF4 import Dataset
+import glob
+import os
+
+h1s   = ["Ecosystem and Carbon Cycle","Hydrology Cycle"]
+h2s   = {"Ecosystem and Carbon Cycle":["gpp","nee","reco"],
+         "Hydrology Cycle"           :["le","sh"]}
+clrs  = {"Ecosystem and Carbon Cycle":"#ECFFE6",
+         "Hydrology Cycle"           :"#E6F9FF"}
+names = {"gpp" :"Gross Primary Productivity",
+         "nee" :"Net Ecosystem Exchange",
+         "reco":"Ecosystem Respiration",
+         "le"  :"Latent Heat",
+         "sh"  :"Sensible Heat"}
+
+for h1 in h1s:    
+    print("\n[h1: %s]" % h1)
+    print('bgcolor = "%s"' % clrs[h1])
+    for vname in h2s[h1]:
+        print("\n[h2: %s]" % names[vname])
+        print("variable       = %s" % vname)
+        possible = [vname.upper()]
+        dsetstr = ""
+        for root,subdirs,files in os.walk(os.path.join(os.environ['ILAMB_ROOT'],"DATA")):
+            files.sort()
+            for fname in files:
+                if not fname.endswith(".nc"): continue
+                pname = os.path.join(root,fname)
+                rname = pname.replace("%s" % os.environ['ILAMB_ROOT'],"")
+                dname = fname.replace(".nc","")
+                if rname.startswith("/"): rname = rname[1:]
+                with Dataset(pname) as dset:
+                    poss = [v for v in dset.variables.keys() if v.split("_")[0] == vname.upper()]
+                    if poss:
+                        possible += [v for v in poss if ((v not in possible) and (v != vname))]
+                        dsetstr  += "\n[%s]" % dname
+                        dsetstr  += "\nsource = %s\n" % rname
+        print("alternate_vars = %s" % (",".join(possible)))
+        print("ctype          = ConfPEcAn")
+        print(dsetstr)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PEcAnILAMB
-Assets for collaborative work linking PEcAn and ILAMB
+Assets for collaborative work linking PEcAn and ILAMB.
+
+The following is a brief overview of what is in the directories.
+
+- AMFParsing: scripts for converting AMF csv/xls files into CF-compliant netCDF4 files
+- ILAMB: confrontation and config file for ILAMB, as well as a script which can autogenerate that config file for if and when we want to expand
+
